c9f5afdd51df4a822e0ef71b65d53bc872c70992
guy djoumessi <gmdnegocio@gmail.com>
2025-03-10 20:11:33 -0300

    Initial commit

---
+++ voudefone/src/test/java/com/voudefone/VoudefoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voudefone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoudefoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
